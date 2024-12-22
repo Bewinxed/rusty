e3c7fe65cc2e183259b12a8e068d26c01000db81
Bewinxed <bewinxed@gmail.com>
2024-12-22 16:18:16 +0300

    Add initial project files and configurations

---
+++ src/printy.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let name = "'uwu'";
+    println!("my name is {name}");
+    println!("my name is {name}");
+    let number = 31;
+    println!("this number is {number}");
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+}
